1999687176c4439da2653b62f53b369e96c87d42
AmirbahadorRostami <33916275+AmirbahadorRostami@users.noreply.github.com>
2022-01-13 14:48:08 -0500

    update

---
+++ control.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function(){    
+    
+
+    $(".panel").hide();
+
+    $(".nav-link").click(function(){
+        
+        $(".nav-link.active").removeClass("active");
+        $(this).addClass("active");
+        
+        var panelToShow = $(this).attr("rel");
+            
+        $(".panel.active").slideUp("500", function(){
+            $(this).removeClass("active");
+                    
+                    $('#'+panelToShow).slideDown("500",function(){
+                            $(this).addClass("active");        
+                    });
+            });
+
+    });
+});
+
+
+
+    
+               
+                  
+                  
